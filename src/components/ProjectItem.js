8ecd383cc36ed2551b03d380ae97a3ac0d92e163
moses <moses.kingi@moringaschool.com>
2023-11-25 09:31:54 +0300

    list and keys completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem(props) {
+  const {name, about ,technologies} = props;
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-      {technologies.map((technology) => (
-          <span key={technology}>{technology}</span>
-          ))}
+        {technologies.map((tech, index) => (
+          <span key={index}>{tech}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,9 +2,9 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
   const projectItems = projects.map((project) => (
-    <ProjectItem key={project.id} project={project} />
+    <ProjectItem key={project.id} name={project.name} about={project.about}  technologies={project.technologies} />
   ));
 
   return (
